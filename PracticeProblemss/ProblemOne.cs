02182415b81fec17e4205515e10b5e2cae09e9fc
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-07-30 14:25:10 -0500

    problems 1 and 2 are done, in the middle of problem 3

--- PracticeProblemss/ProblemOne.cs
+++ PracticeProblemss/ProblemOne.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PracticeProblemss
 {
+
+    //1. Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+    //Create Delegate that will find "th" in each string in List(words)
+    //yield return to pass up strings that do not fit criteria(strings in List that do not have "th")
+    //once found create lambda epxression to print those strings out
+
     class ProblemOne
     {
+       
+
+
     }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
--- PracticeProblemss/Program.cs
+++ PracticeProblemss/Program.cs
@@ -10,7 +10,38 @@ namespace PracticeProblemss
     {
         static void Main(string[] args)
         {
+            //Problem 1
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsWithTH = words.Where(w => w.Contains("th")).ToList();
+
+
+            //Problem 2
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var oneMike = names.Distinct().ToList();
+
+            //Problem 3 //to separate each class list of grades use for each
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+            foreach (var student in classGrades)
+            {
+                //double.Parse(Console.WriteLine(student));
+                
+
+                var studentGrades = student.Split(',').ToArray().;
+
+
+            }
+                
+
             
+
         }
+
+        
     }
 }
